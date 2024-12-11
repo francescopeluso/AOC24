f6613e3506c18189783033111da8f0bf14d01125
Francesco Peluso <59664699+francescopeluso@users.noreply.github.com>
2024-12-11 21:51:39 +0100

    Day 11 optimized af

--- day11/main.py
+++ day11/main.py
@@ -1,57 +1,30 @@
-#
-#   Advent of Code 2024 - Day 10
-#   Francesco Peluso - @francescopeluso on GitHub
-#   Repo:         https://github.com/francescopeluso/AOC24
-#   My website:   https://francescopeluso.xyz
-#
-
+from functools import cache
 
 def read_input():
-  with open('input.txt', 'r') as file:
-    return [num for num in file.readline().strip().split(" ")]
+  with open('input.txt', 'r') as f:
+    return list(map(int, f.read().split()))
 
+@cache
+def count_stones(stone, it):
+  if it == 0:
+    return 1
+  if stone == 0:
+    return count_stones(1, it-1)
+  
+  s = str(stone)
+  l = len(s)
+  if l % 2 == 1:
+    return count_stones(stone * 2024, it-1)
 
-def rearrange_stones(stones):
-  new_stones = []
-
-  for stone in stones:
-
-    if stone == "0":
-      new_stones.append("1")
-
-    elif len(stone) % 2 == 0:
-      half_len = len(stone) // 2
-      left_stone = str(int(stone[:half_len]))
-      right_stone = str(int(stone[half_len:]))
-      new_stones.append(left_stone)
-      new_stones.append(right_stone)
-
-    else:
-      new_stones.append(str(int(stone) * 2024))
-
-  return new_stones
-
+  return count_stones(int(s[: l // 2]), it-1) + count_stones(int(s[l // 2:]), it-1)          
 
 def first_part(stones):
-  current_stones = stones.copy()
-
-  for _ in range(25):
-    current_stones = rearrange_stones(current_stones)
-
-  return len(current_stones)
-
+  return sum(map(lambda s: count_stones(s, 25), stones))
 
 def second_part(stones):
-  current_stones = stones.copy()
-
-  for _ in range(75):
-    current_stones = rearrange_stones(current_stones)
-    print("Actual length:", _, len(current_stones))
-
-  return len(current_stones)
+  return sum(map(lambda s: count_stones(s, 75), stones))
 
 if __name__ == "__main__":
   stones = read_input()
-    
   print("First part result:", first_part(stones))
   print("Second part result:", second_part(stones))
